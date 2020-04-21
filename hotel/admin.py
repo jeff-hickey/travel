fc3d6883448ed7bc56316e3c51adf041f15d8559
jeffhickey <hickey.jeff@gmail.com>
2020-04-20 23:28:47 -0700

    first commit.

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from hotel.models import User, Room, Amenity, Hotel
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("create_date", "label")
+
+
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ("create_date", "label")
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("create_date", "label", "num_beds", "number_on_door")
+
+
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Hotel, HotelAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Amenity, AmenityAdmin)
--- hotel/models.py
+++ hotel/models.py
@@ -4,3 +4,29 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Amenity(models.Model):
+    create_date = models.DateTimeField(auto_now=True)
+    code = models.CharField(max_length=2)
+    label = models.TextField(max_length=64)
+
+
+class Hotel(models.Model):
+    create_date = models.DateTimeField(auto_now=True)
+    label = models.TextField(max_length=64)
+    about_info = models.TextField(max_length=10000)
+    amenity_list = models.ForeignKey(Amenity, on_delete=models.CASCADE,
+                                     related_name='amenity_list')
+    room_list = models.ForeignKey(Amenity, on_delete=models.CASCADE,
+                                  related_name='room_list')
+
+
+class Room(models.Model):
+    create_date = models.DateTimeField(auto_now=True)
+    label = models.TextField(max_length=64)
+    about_info = models.TextField(max_length=5000)
+    number_on_door = models.PositiveIntegerField()
+    floor = models.PositiveIntegerField()
+    num_beds = models.PositiveIntegerField(default=1)
+    max_guests = models.PositiveIntegerField(default=1)
