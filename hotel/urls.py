f7b4530b7d295a3ff5c48afaa6d689af8385549c
jeffhickey <hickey.jeff@gmail.com>
2020-04-20 22:07:26 -0700

    first commit.

---
+++ hotel/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # Account creation and login.
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register")
+]
--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,60 @@
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # Default route for the Hotel website.
+    return render(request, "hotel/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "hotel/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "hotel/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "hotel/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "hotel/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "hotel/register.html")
