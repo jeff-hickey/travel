2983a145b1d644ef14ff85dba1ba9c5c0680e5b6
jeffhickey <hickey.jeff@gmail.com>
2020-04-28 20:14:31 -0700

    Static files dir to enable deployment.

--- travel/settings.py
+++ travel/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticdeploy')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'hotel/static'),
 )
 
 # Quick-start development settings - unsuitable for production
