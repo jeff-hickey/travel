dd2b8b3891a57f1c3760eb7396376d108d06c3ea
jeffhickey <hickey.jeff@gmail.com>
2020-05-05 14:22:17 -0700

    Heroku Deployment.

--- travel/settings.py
+++ travel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #
-# SECRET_KEY = 'm(48b%az3hg$$s*cforw@adf3#w2u6oq97(um75ztx&st!9mu='
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
