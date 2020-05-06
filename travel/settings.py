fd5a72dd54e44bb18c3015683cf94b711f04d0d2
jeffhickey <hickey.jeff@gmail.com>
2020-05-05 21:47:17 -0700

    Heroku Deployment.

--- travel/settings.py
+++ travel/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,4 +123,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if '/app' in os.environ['HOME']:
     import django_heroku
+
     django_heroku.settings(locals())
