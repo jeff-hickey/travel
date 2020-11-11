69c85622c73c22a13b0f444c03df5719336032cc
jeffhickey <hickey.jeff@gmail.com>
2020-11-11 14:50:57 -0700

    Updated the title.

--- hotel/views.py
+++ hotel/views.py
@@ -425,3 +425,24 @@ def register(request):
     else:
         return render(request, "hotel/register.html",
                       {"register_page": "active"})
+
+
+def login(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page:
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        # Otherwise, return login page again with new context
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials"
+            })
+    return render(request, "users/login.html")
